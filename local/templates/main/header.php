dd3fc0c966340bd8f7f3205a5804a189b5be0397
Artyom Feoktistov <a.feoktistov@wptt.ru>
2021-07-28 14:18:18 +0500

    Initial commit

---
+++ local/templates/main/header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+
+<html lang="<?= LANGUAGE_ID; ?>">
+<head>
+    <?php $APPLICATION->ShowHead(); ?>
+
+    <title><?php $APPLICATION->ShowTitle(); ?></title>
+    <meta charset="UTF-8">
+    <link rel="icon" type="image/png" href="<?= SITE_TEMPLATE_PATH; ?>/favicon/favicon.ico" />
+</head>
+
+<body>
+<?php $APPLICATION->ShowPanel();?>
+
+
---
+++ sites/s1/index.php
@@ -0,0 +1,1 @@
+<?    require($_SERVER['DOCUMENT_ROOT'].'/bitrix/header.php');    $APPLICATION->SetTitle("Прознание");?><?    require($_SERVER['DOCUMENT_ROOT'].'/bitrix/footer.php');?>
